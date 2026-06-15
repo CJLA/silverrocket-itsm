0db4ec6b26545fdd2662888f8067dcdae04fd3d1
Christopher Johnson <39108639+CJLA@users.noreply.github.com>
2026-06-14 22:40:02 -0500

    Add user registration API endpoint
    * Add user registration API endpoint and tests
    
    * Fix registration email normalization and password validation

--- silverrocket/accounts/views.py
+++ silverrocket/accounts/views.py
@@ -1,1 +1,9 @@
-# Create your views here.
+from rest_framework import generics
+
+from .models import CustomUser
+from .serializers import UserRegistrationSerializer
+
+
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegistrationSerializer
